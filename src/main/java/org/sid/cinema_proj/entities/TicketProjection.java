f44563c9198dc4f489f405a3fc0dbdda7e068a06
ALAA ADOUI <alaaadoui@gmail.com>
2020-05-26 05:49:23 +0000

    Add interface for projections and tickets

--- src/main/java/org/sid/cinema_proj/entities/TicketProjection.java
+++ src/main/java/org/sid/cinema_proj/entities/TicketProjection.java
@@ -1,7 +1,7 @@
 package org.sid.cinema_proj.entities;
 
 import org.springframework.data.rest.core.config.Projection;
-
+//http://localhost:8086/projectionFilms/1/tickets?projection=ticketProj
 @Projection(name = "ticketProj",types = {org.sid.cinema_proj.entities.Ticket.class})
 public interface TicketProjection {
     public Long getId();
